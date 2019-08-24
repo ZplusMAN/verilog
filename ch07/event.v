6a638a18cf483e52ef8770217f26af7e29c7f33e
ZplusMAN <zplus@aliyun.com>
2019-08-24 20:15:25 +0800

    review ch07

---
+++ ch07/event.v
@@ -0,0 +1,12 @@
+module demo;
+	event received_data ;
+
+	always @( posedge clock ) 
+	begin 
+		if( last_data_packet )
+			->received_data ;
+	end 
+
+	always @(received_data )
+		data_buf = data ;
+endmodule 
